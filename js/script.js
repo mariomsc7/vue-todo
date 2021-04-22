ff03e43de55101e6c3a299ce83bb2f8ee42c9359
mariomsc7 <mariomiscischia@gmail.com>
2021-04-22 14:59:50 +0200

    stampa todos a schermo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Vue', Vue);
+/**
+ * VUE TO DO LIST
+ */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {
+                text: 'Fare i compiti',
+                completed: false,
+            },
+            {
+                text: 'Fare la spesa',
+                completed: false,
+            },
+            {
+                text: 'Fare il bucato',
+                completed: false,
+            },
+        ],
+        newTodo: '',
+    },
+    methods: {
+        addTodo() {
+            console.log('add');
+        }
+    }
+});
